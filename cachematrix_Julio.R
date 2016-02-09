4a155be88a743da5d3b65297fe247450d6911f40
arnoldinrj <arnldin@gmail.com>
2016-02-09 10:30:22 -0200

    CacheMatrix_Julio_2_0
    Two functions to the ProgrammingAssignment 2

---
+++ cachematrix_Julio.R
@@ -0,0 +1,38 @@
+@@ -4,12 +4,32 @@
+## The first function is responsible for storing 
+## the matrix that will be cached for another function 
+
+  
+  makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
+
+
+
+## This function will use the cached data to 
+## define if is necessary or not to calculate the inverse matrix.
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
